0508841a92bcd1d69209be01bfe7223904d2a7e9
Dakson Cruz <daksonc@gmail.com>
2022-10-01 14:43:39 -0300

    feat: add axios

---
+++ src/shared/services/api/axios-config/interceptor/ErrorInterceptor.ts
@@ -0,0 +1,12 @@
+import { AxiosError } from 'axios';
+
+export const errorInterceptor = (error: AxiosError) => {
+  if (error.message === 'Network Error') {
+    return Promise.reject(new Error('Falha na conexão com o servidor! :('));
+  }
+  if (error.response?.status === 401) {
+    // Do something
+  }
+
+  return Promise.reject(error);
+};

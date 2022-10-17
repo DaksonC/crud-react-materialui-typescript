59c300ddfb24aad761b5c18bd345c48c47f2acda
Dakson Cruz <daksonc@gmail.com>
2022-10-17 11:12:17 -0300

    feat: cria contexto de autenticação

---
+++ src/shared/contexts/AuthContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useCallback, useEffect, useMemo, useState } from "react"
+import { AuthService } from "../services/api/auth/AuthService";
+
+interface IAuthContextData {
+  logout: () => void;
+  isAuthenticated: boolean;
+  login: (email: string, password: string) => Promise<string | void>;
+}
+
+interface IAuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthContext = createContext({} as IAuthContextData);
+
+export const AuthProvider = ({ children }: IAuthProviderProps) => {
+  const [accessToken, setAccessToken] = useState<string>();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("App_accessToken");
+    if (accessToken) {
+      setAccessToken(JSON.parse(accessToken));
+    } else {
+      setAccessToken(undefined);
+    }
+  }, []);
+
+  const handleLogin = useCallback(async (email: string, password: string) => {
+    const result = await AuthService.auth(email, password);
+
+    if (result instanceof Error) {
+      return result.message;
+    } else {
+      localStorage.setItem('APP_accessToken', JSON.stringify(result.access_token));
+      setAccessToken(result.access_token);
+    }
+
+  }, []);
+
+  const handleLogout = useCallback(() => {
+    localStorage.removeItem('APP_accessToken');
+    setAccessToken(undefined);
+  }, []);
+
+  const isAuthenticated = useMemo(() => !!accessToken, [accessToken]);
+
+  return (
+    <AuthContext.Provider value={{ logout: handleLogout, isAuthenticated, login: handleLogin }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
